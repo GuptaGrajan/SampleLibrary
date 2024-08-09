d9a209a229d439fa217abc9e0285e8e97242b33c
Akshay Khajuria <akshay.khajuria@techglock.com>
2024-08-09 17:50:00 +0530

    setupSDK

--- sampleApp/build.gradle.kts
+++ sampleApp/build.gradle.kts
@@ -25,7 +25,7 @@ android {
     defaultConfig {
 //        applicationId = "com.example.baseproject"
         minSdk = 24
-        targetSdk = 34
+//        targetSdk = 34
 //        versionCode = 1
 //        versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
