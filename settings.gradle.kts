6510f97814e9daa56f383d9b5c2850fc8142a601
GuptaGrajan <rggk13@gmail.com>
2024-09-25 19:10:09 +0530

    code clean

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,6 +20,17 @@ dependencyResolutionManagement {
         mavenCentral()
         jcenter()
 
+        mavenLocal()
+
+        // OR publish to a remote repository, e.g., JFrog Artifactory, Nexus
+        maven {
+            url = uri("https://github.com/GuptaGrajan/SampleLibrary.git") // Replace with your repository URL
+            credentials {
+                username = settings.extra["repo.user"].toString()
+                password = settings.extra["repo.password"].toString()
+            }
+        }
+
     }
 }
 
